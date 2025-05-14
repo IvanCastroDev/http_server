4032bff3666f1010777204c10626b8d9a6c00c77
IvanCastro19 <79170316+IvanCastro19@users.noreply.github.com>
2025-05-14 14:41:35 -0600

    Add regex and memchr dependencies; refactor request handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Imported libraries
 use std::{
     io::{
         BufRead, BufReader, Write
@@ -12,87 +13,120 @@ use std::sync::Mutex;
 #[allow(unused_imports)]
 use anyhow::Error;
 
+// Structs and types
+type RouteFunction = Box<dyn Fn(&Request) -> String + Send + Sync>;
+
 #[derive(Debug)]
 struct Request {
     method: String,
     route: String,
-    headers: Vec<String>
+    headers: Vec<String>,
+    stream: TcpStream
 }
 
-fn echo_request(stream: &TcpStream) {
+impl Request  {
+    fn new (mut s: TcpStream) -> Self {
+        // We use bufReader to read the stream bytes in fragments (8kib).
+        let mut reader = BufReader::new(&mut s);
+        let mut request_data = String::new();
 
+        loop {
+            // String as a bufer to translate the bytes read in a string
+            let mut str_buf = String::new();
+
+            // We read and count the amount of bytes in the stream before a crlf that marks the end of a line
+            let bytes_read = reader.read_line(&mut str_buf).unwrap();
+
+            // If the reads are equal to zero it is because the connection with the client is closed
+            if bytes_read == 0 {
+                break;
+            };
+
+            // End of the headers and request line 
+            if str_buf == "\r\n" {
+                break;
+            };
+            
+            // We add the new line to the string that stores all the data from the request
+            request_data.push_str(&str_buf);
+        }
+
+        // We separate the data to divide them in [request line, headers]
+        let request_data: Vec<String> = request_data
+            .split("\r\n")
+            .map(|s| s.to_string())
+            .collect();
+
+        // We extract the data from the petition line to be able to throw the method and the destination route
+        let start_line = &request_data[0];
+        let start_line_parts: Vec<&str> = start_line.split(" ").collect();
+
+
+        // We return a struct with the formatted request data
+        Request {
+            method: start_line_parts[0].to_string(),
+            route: start_line_parts[1].to_string(),
+            headers: request_data,
+            stream: s
+        }
+    }
 }
 
-static ROUTES: Lazy<Mutex<HashMap<&'static str, Box<dyn Fn(&TcpStream) + Send + Sync>>>> = Lazy::new(|| {
-    #[warn(unused_mut)]
-    let mut routes: HashMap<&'static str, Box<dyn Fn(&TcpStream) + Send + Sync>> = HashMap::new();
+static ROUTES: Lazy<Mutex<HashMap<&'static str, RouteFunction>>> = Lazy::new(|| {
+    #[allow(unused_mut)]
+    let mut routes: HashMap<&'static str, RouteFunction> = HashMap::new();
 
     Mutex::new(routes)
 });
 
-fn handle_request(mut stream: TcpStream) {
-    let request = read_stream(&stream);
-    println!("Request data\nmethod: {}\nroute: {}\nheaders: {:?}", request.method, request.route, request.headers);
+// Utilities
+fn register_route(route: &'static str, f: RouteFunction) {
+    let mut routes = ROUTES.lock().unwrap();
 
-    let mut response = String::from("HTTP/1.1 ");
-    
-    match request.route {
-        route if route == "/" => &response.push_str("200 OK\r\n\r\n"),
-        _ => &response.push_str("404 Not Found\r\n\r\n"),
-    };
-
-    stream.write(response.as_bytes()).unwrap();
+    routes.insert(route, f);
 }
 
-/// Method to translate the request data
-fn read_stream(stream: &TcpStream) -> Request {
-    // We use bufReader to read the stream bytes in fragments (8kib).
-    let mut reader = BufReader::new(stream);
-    let mut request_data = String::new();
+fn exec_route_function(name: &str, request: &mut Request) {
+    let routes = ROUTES.lock().unwrap();
 
-    loop {
-        // String as a bufer to translate the bytes read in a string
-        let mut str_buf = String::new();
+    let mut response = String::from("HTTP/1.1 ");
 
-        // We read and count the amount of bytes in the stream before a crlf that marks the end of a line
-        let bytes_read = reader.read_line(&mut str_buf).unwrap();
+    match routes.get(name) {
+        Some(f) => {
+            response.push_str(&f(request));
+        },
+        None => response.push_str("404 Not Found\r\n\r\n")
+    };
 
-        // If the reads are equal to zero it is because the connection with the client is closed
-        if bytes_read == 0 {
-            break;
-        };
+    request.stream.write(response.as_bytes()).unwrap();
+}
 
-        // End of the headers and request line 
-        if str_buf == "\r\n" {
-            break;
-        };
-        
-        // We add the new line to the string that stores all the data from the request
-        request_data.push_str(&str_buf);
-    }
+fn handle_request(stream: TcpStream) {
+    let mut request = Request::new(stream);
+    println!("Request data\nmethod: {}\nroute: {}\nheaders: {:?}", request.method, request.route, request.headers);
+    exec_route_function("/echo", &mut request);
+}
 
-    // We separate the data to divide them in [request line, headers]
-    let request_data: Vec<String> = request_data
-        .split("\r\n")
+//Functions for routes destinations
+fn echo(request: &Request) -> String {
+    let split_route = request.route
+        .split("/")
+        .filter(|s| !s.is_empty())
         .map(|s| s.to_string())
-        .collect();
+        .collect::<Vec<String>>();
 
-    // We extract the data from the petition line to be able to throw the method and the destination route
-    let start_line = &request_data[0];
-    let start_line_parts: Vec<&str> = start_line.split(" ").collect();
+    println!("Message in request: {:?}", split_route);
 
+    let message: &String = &split_route[1];
 
-    // We return a struct with the formatted request data
-    Request {
-        method: start_line_parts[0].to_string(),
-        route: start_line_parts[1].to_string(),
-        headers: request_data
-    }
+    String::from(format!("200 Ok\r\nContent-Type:text/plain\r\nContent-Length: {}\r\n\r\n{}", message.len(), message))
 }
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+
+    register_route("/echo", Box::new(echo));
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
